236692748e69b619268801bcb44d9631cec4529a
Alperen <alperensafak@gmail.com>
2021-10-19 17:34:31 +0300

    resource service created

--- tools/model/user.model.ts
+++ tools/model/user.model.ts
@@ -1,4 +1,7 @@
 import { AuditModel } from './audit.model';
+import { RoleModel } from './role.model';
+import { GroupModel } from './group.model';
+import * as mongoose from 'mongoose'
 
 export class UserModel {
   id:string;
@@ -9,4 +12,20 @@ export class UserModel {
   password:string;
 audit:AuditModel;
 birthDay: Date
+  roles: RoleModel[];
+groups:GroupModel[];
 }
+
+
+export const UserSchema = new mongoose.Schema({
+
+  name:String,
+  surname:String,
+  image:String,
+  email:String,
+  password:String,
+  birthDay: Date,
+  audit:Object,
+  roles:Array,
+  groups:Array,
+})
